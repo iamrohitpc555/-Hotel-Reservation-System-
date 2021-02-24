5b2eeb2847c3ac64b8c404b8bab4fa2162bf5057
iamrohitpc555 <35221090+iamrohitpc555@users.noreply.github.com>
2021-02-25 01:09:00 +0530

    Update HotelReservation.java

--- src/main/java/org/example/HotelReservation.java
+++ src/main/java/org/example/HotelReservation.java
@@ -6,33 +6,37 @@ import java.util.Scanner;
 
 public class HotelReservation {
 
-    public static List<Hotel> hotelList = new ArrayList<Hotel>();
+	public static List<Hotel> hotelList = new ArrayList<Hotel>();
 
-    public static void addRegularRateAndHotelName(String hotelName, int regularRate) {
-        Hotel hotel = new Hotel(hotelName, regularRate);
-        hotelList.add(hotel);
-    }
+	public static void addRateAndHotelName(String hotelName, int weeklyRate, int weekEndRate) {
+		Hotel hotel = new Hotel(hotelName, weeklyRate, weekEndRate);
+		hotelList.add(hotel);
+	}
 
-    public static void main(String[] args) {
+	public static void findCheapestHotel(String startDate, String endDate) {
+		LocalDate start = LocalDate.parse(startDate);
+		LocalDate end = LocalDate.parse(endDate);
 
-        System.out.println("Welcome to Hotel Reservation Program");
+		Hotel cheapestHotel = null;
+		int minRate = 1000000;
 
-        Scanner sc = new Scanner(System.in);
+		for (Hotel hotel : hotelList) {
+			LocalDate tempStart = start;
+			LocalDate tempEnd = end.plusDays(1);
+			int hotelRent = 0;
+			while (!(tempStart.equals(tempEnd))) {
+				hotelRent = hotelRent + hotel.getWeeklyRate();
+				tempStart = tempStart.plusDays(1);
+			}
+			if (hotelRent < minRate) {
+				minRate = hotelRent;
+				cheapestHotel = hotel;
+			}
 
-        while (true) {
-            System.out.println("Enter the hotel name for adding rates: ");
-            String hotelName = sc.next();
-            System.out.println("Enter the Regular rate: ");
-            int regularRate = sc.nextInt();
-            HotelReservation.addRegularRateAndHotelName(hotelName, regularRate);
-            System.out.println("Add more hotel- (Yes/No)");
-            String choice = sc.next();
-            if (choice.equalsIgnoreCase("Yes"))
-                continue;
-            else
-                break;
-        }
+		}
+		System.out.println("The Cheapest option is: ");
+		System.out.println(cheapestHotel.getHotelName() + ", total rent :- " + minRate);
 
-        sc.close();
-    }
+	}
+
 }
