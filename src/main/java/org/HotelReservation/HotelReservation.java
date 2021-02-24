52083e699f1ccc07da0db377a0b49180894955b1
iamrohitpc555 <35221090+iamrohitpc555@users.noreply.github.com>
2021-02-24 17:55:38 +0530

    Ability to add Hotel in a Hotel Reservation System with Name and rates for Regular Customer...

--- src/main/java/org/HotelReservation/HotelReservation.java
+++ src/main/java/org/HotelReservation/HotelReservation.java
@@ -1,4 +1,4 @@
-package org.example;
+package org.HotelReservation;
 
 import java.util.ArrayList;
 import java.util.List;
--- src/test/java/org/HotelReservation/AppTest.java
+++ src/test/java/org/HotelReservation/AppTest.java
@@ -1,4 +1,4 @@
-package org.example;
+package org.HotelReservation;
 
 import static org.junit.Assert.assertTrue;
 
