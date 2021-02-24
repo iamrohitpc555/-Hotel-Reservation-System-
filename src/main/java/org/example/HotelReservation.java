e85e874ce21c4f2c8196f14c7280a8121ca8e55b
iamrohitpc555 <35221090+iamrohitpc555@users.noreply.github.com>
2021-02-25 01:06:42 +0530

    Update HotelReservation.java

--- src/main/java/org/example/HotelReservation.java
+++ src/main/java/org/example/HotelReservation.java
@@ -12,6 +12,34 @@ public class HotelReservation {
         Hotel hotel = new Hotel(hotelName, regularRate);
         hotelList.add(hotel);
     }
+    
+    public static void findCheapestHotel(String startDate, String endDate) {
+		LocalDate start = LocalDate.parse(startDate);
+		LocalDate end = LocalDate.parse(endDate);
+
+		Hotel cheapestHotel = null;
+		int minRate = 1000000;
+
+		for (Hotel hotel : hotelList) {
+			LocalDate tempStart = start;
+			LocalDate tempEnd = end.plusDays(1);
+			int hotelRent = 0;
+			while (!(tempStart.equals(tempEnd))) {
+				hotelRent = hotelRent + hotel.getWeeklyRate();
+				tempStart = tempStart.plusDays(1);
+			}
+			if (hotelRent < minRate) {
+				minRate = hotelRent;
+				cheapestHotel = hotel;
+			}
+
+		}
+		System.out.println("The Cheapest option is: ");
+		System.out.println(cheapestHotel.getHotelName() + ", total rent :- " + minRate);
+
+	}
+
+}
 
     public static void main(String[] args) {
 
