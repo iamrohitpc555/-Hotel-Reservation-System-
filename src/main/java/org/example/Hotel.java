e65d50bdd99bafae4a920791b14d8b7c2bc5837a
iamrohitpc555 <35221090+iamrohitpc555@users.noreply.github.com>
2021-02-25 01:24:24 +0530

    Update Hotel.java

--- src/main/java/org/example/Hotel.java
+++ src/main/java/org/example/Hotel.java
@@ -1,32 +1,69 @@
 package org.example;
 
-public class Hotel
-{
-    private String hotelName;
-    private int regularRate;
+public class Hotel {
 
-    public Hotel(String hotelName , int regularRate)
-    {
-        this.hotelName= hotelName;
-        this.regularRate= regularRate;
-    }
+	private String hotelName;
+	private int rating;
+	private int weeklyRate;
+	private int weekEndRate;
+	private int weeklyRateReward;
+	private int weekEndRateReward;
 
-    public void setHotelName(String hotelName)
-    {
-        this.hotelName = hotelName;
-    }
+	public Hotel(String hotelName, int rating, int weeklyRate, int weekEndRate, int weeklyRateReward,
+			int weekEndRateReward) {
+		this.hotelName = hotelName;
+		this.rating = rating;
+		this.weeklyRate = weeklyRate;
+		this.weekEndRate = weekEndRate;
+		this.weeklyRateReward = weeklyRateReward;
+		this.weekEndRateReward = weekEndRateReward;
+	}
 
-    public String getHotelName()
-    {
-        return hotelName;
-    }
+	public void setHotelName(String hotelName) {
+		this.hotelName = hotelName;
+	}
 
-    public void setRegularRate(int regularRate)
-    {
-        this.regularRate = regularRate;
-    }
+	public String getHotelName() {
+		return hotelName;
+	}
 
-    public int getRegularRate() {
-        return regularRate;
-    }
+	public void setRating(int rating) {
+		this.rating = rating;
+	}
+
+	public int getRating() {
+		return rating;
+	}
+
+	public void setWeeklyRate(int weeklyRate) {
+		this.weeklyRate = weeklyRate;
+	}
+
+	public int getWeeklyRate() {
+		return weeklyRate;
+	}
+
+	public void setWeekEndRate(int weekEndRate) {
+		this.weekEndRate = weekEndRate;//
+	}
+
+	public int getWeekEndRate() {
+		return weekEndRate;
+	}
+
+	public void setWeeklyRateReward(int weeklyRateReward) {
+		this.weeklyRateReward = weeklyRateReward;
+	}
+
+	public int getWeeklyRateReward() {
+		return weeklyRateReward;
+	}
+
+	public void setWeekEndRateReward(int weekEndRateReward) {
+		this.weekEndRateReward = weekEndRateReward;
+	}
+
+	public int getWeekEndRateReward() {
+		return weekEndRateReward;
+	}
 }
